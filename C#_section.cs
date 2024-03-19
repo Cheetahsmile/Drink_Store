5c4839711f734680745a67336be49f7c33ed2aca
Cheetahsmile <maggiebaker768@gmail.com>
2024-03-19 18:16:02 -0400

    c#

---
+++ C#_section.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class C#_section : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        string[] Welcome = { " welcome To my game", "My game is about a woman trying to escape a store with robots and ghost", "She has a robot companion and this woman's really good at fighting", "So watch out"};
+        Console.WriteLine(welcome[0]);
+        Console.WriteLine(welcome[1]);
+        Console.WriteLine(welcome[2]);
+        Console.WriteLine(welcome[3]);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
