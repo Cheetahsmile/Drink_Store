40696d8dce5a7f6aef70885b5c9f72d17a883770
Cheetahsmile <maggiebaker768@gmail.com>
2024-03-21 16:33:31 -0400

    Loops added

--- C#_section.cs
+++ C#_section.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class C#_section : MonoBehaviour
+public class csharp_script : MonoBehaviour
 {
     // Start is called before the first frame update
+    
     void Start()
-
     {
         string[] Welcome = { " welcome To my game", "My game is about a woman trying to escape a store with robots and ghost", "She has a robot companion and this woman's really good at fighting", "So watch out"};
-        Console.WriteLine(welcome[0]);
-        Console.WriteLine(welcome[1]);
-        Console.WriteLine(welcome[2]);
-        Console.WriteLine(welcome[3]);
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+        for (int i= 0; i<= Welcome.Length;i++)
+        {
+            Debug.Log(Welcome[i]);
+            //Console.WriteLine(Welcome[i]);
+        }
+    }       
+      
